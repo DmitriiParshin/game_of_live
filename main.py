0ad6f02bd0c7f47a0a89aef1dc8831c55b85f530
DmitriiParshin <94290719+DmitriiParshin@users.noreply.github.com>
2021-12-16 10:57:59 +0200

    + counter
    game.counter

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ def index():
 @app.route('/live')
 def live():
     game = GameOfLife()
-    game.form_new_generation()
-    return render_template('live.html', game=game)
+    if game.counter > 0:
+        game.form_new_generation()
+    game.counter += 1
+    return render_template('live.html', game=game, counter=game.counter)
+
+
 
 
 if __name__ == "__main__":
